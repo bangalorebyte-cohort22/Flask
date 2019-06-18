0d1a6b2189865e5fa2d53b8609b4858420f3d8ed
Ujwal-Sharma <50569208+Ujwal-Sharma@users.noreply.github.com>
2019-06-18 12:32:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/',methods = ['GET','POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def show():
     if request.method == 'POST':
         text = request.form['name']
-        return render_template('index.html', text=text , num=5)
-    return render_template('index.html', text='Stranger' , num=5 )
+        return render_template('index.html', text=text, num=5)
+    return render_template('index.html', text='Stranger', num=5 )
 
 if __name__ == "__main__":
     app.run(debug=True)
