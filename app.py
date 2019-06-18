a1ca924c677ec39409ce71da203ee2ab45534be3
datapsyche <jithinjkumar@gmail.com>
2019-06-18 12:07:54 +0530

    form

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/show')
-def show_something():
-    show = 'goodbye'
-    if show == 'goodbye':    
-        return render_template('goodbye.html')
-    return render_template('hello.html')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/',methods = ['GET','POST'])
+def show():
+    if request.method == 'POST':
+        text = request.form['name']
+        return render_template('index.html', text=text)
+    return render_template('index.html', text='Stranger')
 
 if __name__ == "__main__":
     app.run(debug=True)
